3527cf336523a81c0e39c824ea3279c5c12e1a86
root <root@localhost.localdomain>
2019-05-09 15:16:22 -0400

    Implementing a seperate thread call for decoder

--- src/main/java/uk/elliotalexander/DecoderThread.java
+++ src/main/java/uk/elliotalexander/DecoderThread.java
@@ -1,4 +1,24 @@
 package uk.elliotalexander;
 
-public class DecoderThread {
+import com.google.common.io.BaseEncoding;
+import com.google.common.util.concurrent.Runnables;
+import org.pcap4j.core.PcapDumper;
+import org.pcap4j.core.PcapHandle;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DecoderThread implements Runnable {
+
+    private final Connection working_connection;
+
+    public DecoderThread(Connection c){
+        this.working_connection = c;
+    }
+
+
+    @Override
+    public void run() {
+        this.working_connection.generateTk();
+    }
 }
