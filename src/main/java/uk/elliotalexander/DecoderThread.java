fe92c63c4892ed0b311b5eba734e1cfd5efbb935
ElliotAlexander <ep1e16@soton.ac.uk>
2019-05-13 07:46:40 -0400

    Changing decoder thread to extend Thread

--- src/main/java/uk/elliotalexander/DecoderThread.java
+++ src/main/java/uk/elliotalexander/DecoderThread.java
@@ -1,21 +1,12 @@
 package uk.elliotalexander;
 
-import com.google.common.io.BaseEncoding;
-import com.google.common.util.concurrent.Runnables;
-import org.pcap4j.core.PcapDumper;
-import org.pcap4j.core.PcapHandle;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
-
-public class DecoderThread implements Runnable {
+public class DecoderThread extends Thread {
 
     private final Connection working_connection;
 
     public DecoderThread(Connection c){
         this.working_connection = c;
     }
-
 
     @Override
     public void run() {
